58d4cc0b0480716866125d728aad12a55f2a09ad
datotakuboi <aronetitong@gmailcom>
2024-08-02 01:29:56 +0800

    m

--- playground.py
+++ playground.py
@@ -1,101 +1,129 @@
+chatbot
+
 import streamlit as st
 from streamlit_option_menu import option_menu
 import firebase_admin
 from firebase_admin import firestore
 from google.cloud import storage
 from firebase_admin import credentials
-from firebase_admin import auth
-from dotenv import load_dotenv
 from urllib.parse import urlparse, unquote
 import os
-import json
-import requests
 import tempfile
-from functools import partial
-import datetime
-import mimetypes
 from pdfminer.high_level import extract_text
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+import datetime
+import requests
+import json
 
-### Functions: Start ###
+#Initialize session_state values
+if "oauth_creds" not in st.session_state:
+    st.session_state["oauth_creds"] = None
+
+# Initialize Firebase SDK
+if not firebase_admin._apps:
+    cred = credentials.Certificate(dict(st.secrets["service_account"]))
+    firebase_admin.initialize_app(cred)
 
 SCOPES = ['https://www.googleapis.com/auth/generative-language.retriever']
 
-def load_creds():
-    """Converts client_secret.json to a credential object.
+@st.experimental_dialog("Google Consent Authentication Link")
+def google_oauth_link(flow):
+    auth_url, _ = flow.authorization_url(prompt='consent')
+    st.write("Please go to this URL and authorize access:")
+    st.markdown(f"[Sign in with Google]({auth_url})", unsafe_allow_html=True)
+    code = st.text_input("Enter the authorization code:")
+    return code
 
-    This function caches the generated tokens to minimize the use of the
-    consent screen.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = None
-            if not st.session_state["is_streamlit_deployed"]:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'connext_chatbot_auth.json', SCOPES)
-            else:
-                # Load client config from Streamlit secrets
-                client_config = {
-                    "installed": {
-                        "client_id": st.secrets["installed"]["client_id"],
-                        "project_id": st.secrets["installed"]["project_id"],
-                        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                        "token_uri": "https://oauth2.googleapis.com/token",
-                        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                        "client_secret": st.secrets["installed"]["client_secret"],
-                        "redirect_uris": ["http://localhost"]
-                    }
-                }
-                # Initiate the flow using the client configuration from secrets
-                flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+def load_creds():
+    """Load credentials from Streamlit secrets and handle them using a temporary file."""
+    # Parse the token data from Streamlit's secrets
+    token_info = {
+        'token': st.secrets["token"]["value"],
+        'refresh_token': st.secrets["token"]["refresh_token"],
+        'token_uri': st.secrets["token"]["token_uri"],
+        'client_id': st.secrets["token"]["client_id"],
+        'client_secret': st.secrets["token"]["client_secret"],
+        'scopes': st.secrets["token"]["scopes"],
+        'expiry': st.secrets["token"]["expiry"]  # Assuming expiry is directly usable
+    }
+
+    # Create a temporary file to store the token data
+    temp_dir = tempfile.mkdtemp()
+    token_file_path = os.path.join(temp_dir, 'token.json')
+    with open(token_file_path, 'w') as token_file:
+        json.dump(token_info, token_file)
+
+    # Load the credentials from the temporary file
+    creds = Credentials.from_authorized_user_file(token_file_path, SCOPES)
+
+    # Refresh the token if necessary
+    if creds and creds.expired and creds.refresh_token:
+        st.toast("Currently refreshing token...")
+        creds.refresh(Request())
+
+        # Optionally update the temporary file with the refreshed token data
+        with open(token_file_path, 'w') as token_file:
+            token_file.write(creds.to_json())
+
     return creds
 
-creds = load_creds()
+# Function to generate a signed URL for a file
+def generate_signed_url(bucket_name, blob_name, service_account_info, expiration=3600):
+    storage_client = storage.Client.from_service_account_info(service_account_info)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
 
-def download_file_to_temp(url):
-    # Create a temporary directory
-    storage_client = storage.Client.from_service_account_info(st.session_state["connext_chatbot_admin_credentials"])
-    bucket = storage_client.bucket('connext-chatbot-admin.appspot.com')
-    temp_dir = tempfile.mkdtemp()
+    url = blob.generate_signed_url(expiration=datetime.timedelta(seconds=expiration))
+    return url
 
-    # Download the file
-    response = requests.get(url)
-    parsed_url = urlparse(url)
-    file_name = os.path.basename(unquote(parsed_url.path))
+# Function to download file from URL to a temporary directory
+def download_file_from_url(url):
+    try:
+        temp_dir = tempfile.mkdtemp()
+        file_name = os.path.basename(urlparse(url).path)
+        temp_file_path = os.path.join(temp_dir, file_name)
 
-    blob = bucket.blob(file_name)
-    
-    # Create the full path with the preferred filename
-    temp_file_path = os.path.join(temp_dir, file_name)
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(temp_file_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            return temp_file_path, file_name
+        else:
+            st.error(f"Failed to download file: {response.status_code}")
+            return None, None
+    except Exception as e:
+        st.error(f"Failed to download file: {e}")
+        return None, None
 
-    # # Save the content to the file
-    # with open(temp_file_path, 'wb') as temp_file:
-    #     temp_file.write(response.content)
-    blob.download_to_filename(temp_file_path)
+# Function to extract text from PDFs
+def get_pdf_text(pdf_files):
+    text = ""
+    for pdf in pdf_files:
+        extracted_text = extract_text(pdf)
+        text += extracted_text
+    return text
 
-    return temp_file_path, file_name
+# Function to split text into chunks
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# Function to create vector store
+def get_vector_store(text_chunks, api_key):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
 def extract_and_parse_json(text):
     # Find the first opening and the last closing curly brackets
@@ -114,7 +142,7 @@ def extract_and_parse_json(text):
         return parsed_json, True
     except json.JSONDecodeError:
         return None, False  # JSON parsing failed
-    
+
 def is_expected_json_content(json_data):
     try:
         # Try to load the JSON data
@@ -125,26 +153,9 @@ def is_expected_json_content(json_data):
     required_keys = ["Is_Answer_In_Context", "Answer"]
 
     if not all(key in data for key in required_keys):
-            return False
+        return False
     
-    return True #All checks passed for the specified type
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        extracted_text = extract_text(pdf)
-        text += extracted_text
-    return text
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-def get_vector_store(text_chunks, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    return True  # All checks passed for the specified type
 
 def get_generative_model(response_mime_type = "text/plain"):
     generation_config = {
@@ -153,13 +164,17 @@ def get_generative_model(response_mime_type = "text/plain"):
     "max_output_tokens": 8192,
     "response_mime_type": response_mime_type
     }
-    genai.configure(credentials=creds)
+
+    if st.session_state["oauth_creds"] is not None:
+        genai.configure(credentials=st.session_state["oauth_creds"])
+    else:
+        st.session_state["oauth_creds"] = load_creds()
+        genai.configure(credentials=st.session_state["oauth_creds"])
 
 
     model = genai.GenerativeModel('tunedModels/connext-wide-chatbot-ddal5ox9d38h' ,generation_config=generation_config) if response_mime_type == "text/plain" else genai.GenerativeModel(model_name="gemini-1.5-flash", generation_config=generation_config)
     print(f"Model selected: {model}")
     return model
-
 
 def generate_response(question, context, fine_tuned_knowledge = False):
 
@@ -219,7 +234,7 @@ def try_get_answer(user_question, context="", fine_tuned_knowledge = False):
             except Exception as e:
                 print(f"Failed to create response for the question:\n{user_question}\n\n Error Code: {str(e)}")
                 max_attempts = max_attempts - 1
-                st.toast(f"Failed to create a response for your query.\n Trying again... Retries left: {max_attempts} attempt/s")
+                st.toast(f"Failed to create a response for your query.\n Error Code: {str(e)} \nTrying again... Retries left: {max_attempts} attempt/s")
                 continue
 
             #Test 2
@@ -249,6 +264,7 @@ def try_get_answer(user_question, context="", fine_tuned_knowledge = False):
 
     return parsed_result
 
+# Function to process user input
 def user_input(user_question, api_key):
     
     with st.spinner("Processing..."):
@@ -263,23 +279,31 @@ def user_input(user_question, api_key):
         print(f"Parsed Result: {parsed_result}")
     
     return parsed_result
-    
 
+# Main app function
 def app():
+    st.set_page_config(page_title="Connext Chatbot", layout="centered")
 
-    google_ai_api_key = st.session_state["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"]
-    #Get firestore client
-    firestore_db=firestore.client()
-    st.session_state.db=firestore_db
+    # Retrieve API key from secrets
+    google_ai_api_key = st.secrets["api_keys"]["GOOGLE_AI_STUDIO_API_KEY"]
+
+    # Check if the API key is provided
+    if not google_ai_api_key:
+        st.error("Google API key is missing. Please provide it in the secrets configuration.")
+        return
+
+    # Get firestore client
+    firestore_db = firestore.client()
+    st.session_state.db = firestore_db
 
     # Center the logo image
-    col1, col2, col3 = st.columns([3,4,3])
+    col1, col2, col3 = st.columns([3, 4, 3])
 
     with col1:
         st.write(' ')
 
     with col2:
-        st.image("Connext_Logo.png", width=250) 
+        st.image("Connext_Logo.png", width=250)
 
     with col3:
         st.write(' ')
@@ -322,35 +346,46 @@ def app():
             retriever_description = retriever['retriever_description']
             with st.expander(retriever_name):
                 st.markdown(f"**Description:** {retriever_description}")
-                file_path, file_name = download_file_to_temp(retriever['document']) # Get the document file path and file name
+
+                # Generate signed URL for the document
+                parsed_url = urlparse(retriever['document'])
+                file_name = os.path.basename(unquote(parsed_url.path))
+                signed_url = generate_signed_url('connext-chatbot-admin.appspot.com', file_name, st.secrets["service_account"])
+
                 st.markdown(f"_**File Name**_: {file_name}")
-                retriever["file_path"] = file_path 
-                st.session_state["retrievers"][retriever_name] = retriever #populate the retriever dictionary
+                st.markdown(f"[Download PDF]({signed_url})", unsafe_allow_html=True)
+
+                retriever["signed_url"] = signed_url
+                st.session_state["retrievers"][retriever_name] = retriever  # Populate the retriever dictionary
+
         st.title("PDF Document Selection:")
-        st.session_state["selected_retrievers"] = st.multiselect("Select Documents", list(st.session_state["retrievers"].keys()))  
-        
-        #Get pdf docs of selected retrievers from st.session_state["selected_retrievers"]
+        st.session_state["selected_retrievers"] = st.multiselect("Select Retrievers", list(st.session_state["retrievers"].keys()))
+
         if st.button("Submit & Process", key="process_button"):
             if google_ai_api_key:
                 with st.spinner("Processing..."):
-                    # Get pdf docs of selected retrievers from st.session_state["selected_retrievers"]
-                    selected_files = [st.session_state["retrievers"][name]["file_path"] for name in st.session_state["selected_retrievers"]]
-                    raw_text = get_pdf_text(selected_files)
+                    selected_retrievers = st.session_state["selected_retrievers"]
+                    downloaded_files = []
+                    for name in selected_retrievers:
+                        signed_url = st.session_state["retrievers"][name]["signed_url"]
+                        file_path, _ = download_file_from_url(signed_url)
+                        if file_path:
+                            downloaded_files.append(file_path)
+                    
+                    raw_text = get_pdf_text(downloaded_files)
                     text_chunks = get_text_chunks(raw_text)
                     get_vector_store(text_chunks, google_ai_api_key)
-                    st.success("Done")
+                    st.success("Processing complete.")
             else:
-                st.toast("Failed to process the documents", icon="💥")
+                st.error("Google API key is missing. Please provide it in the secrets configuration.")
 
-    # Assuming you have already defined user_question and google_ai_api_key above this snippet.
-
+    # Process user input and handle responses
     if submit_button:
         if user_question and google_ai_api_key:
             st.session_state.parsed_result = user_input(user_question, google_ai_api_key)
 
     # Setup placeholders for answers
     answer_placeholder = st.empty()
-
 
     if st.session_state.parsed_result is not None and "Answer" in st.session_state.parsed_result:
         answer_placeholder.write(f"Reply:\n\n {st.session_state.parsed_result['Answer']}")
@@ -384,3 +419,6 @@ def app():
         else:
             answer_placeholder.write("Failed to generate a fine-tuned answer.")
         st.session_state["request_fine_tuned_answer"] = False  # Reset the flag after handling
+
+if __name__ == "__main__":
+    app()
